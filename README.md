cd77494ab6eefd9cab81dc00750d6bc7dae02ff9
morris <morrisx28@gmail.com>
2025-11-18 10:21:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Installation Guide
+Follow install guide ref: [unitree_sdk2_python](https://github.com/unitreerobotics/unitree_sdk2_python.git)
+
+## Quick start
+
+Once your computer connect the robot (connect by router)
+```
+python3 pineapple_test.py ur_internet_interface_name
+```
+For example
+```
+python3 pineapple_test.py enxcc96e5abe8c2
+```
+
---
+++ pineapple_test.py
@@ -0,0 +1,279 @@
+import time
+import sys
+import numpy as np
+import threading
+import traceback
+import yaml
+import csv
+import argparse
+import matplotlib.pyplot as plt
+
+from unitree_sdk2py.core.channel import ChannelPublisher, ChannelFactoryInitialize
+from unitree_sdk2py.core.channel import ChannelSubscriber, ChannelFactoryInitialize
+from unitree_sdk2py.idl.default import unitree_go_msg_dds__LowCmd_
+from unitree_sdk2py.idl.default import unitree_go_msg_dds__LowState_
+from unitree_sdk2py.idl.unitree_go.msg.dds_ import LowCmd_
+from unitree_sdk2py.idl.unitree_go.msg.dds_ import LowState_
+from unitree_sdk2py.utils.crc import CRC
+from unitree_sdk2py.utils.thread import RecurrentThread
+
+NUM_MOTORS = 8
+
+class Filter:
+    def __init__(self, alpha):
+        self.filter_value = None
+        self.alpha = alpha
+    
+    def filt(self, input):
+        if self.filter_value is None:
+            self.filter_value = input
+        else:
+            self.filter_value = self.alpha * input + (1 - self.alpha) * self.filter_value
+        return self.filter_value
+
+class Controller:
+    def __init__(self):
+
+        config_file = 'config/pineapple.yaml'
+        with open(f"{config_file}", "r") as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+            self.dt = config["dt"]
+
+            self.kps = np.array(config["kps"], dtype=np.float32)
+            self.kds = np.array(config["kds"], dtype=np.float32)
+
+            self.default_angles = np.array(config["default_angles"], dtype=np.float32)
+            self.sit_angles = np.array(config["sit_angles"], dtype=np.float32)
+            
+            self.cmd_init = np.array(config["cmd_init"], dtype=np.float32)
+
+            num_commands = config["num_commands"]
+            num_actions = config["num_actions"]
+
+            lin_vel_x_range = np.array(config["lin_vel_x_range"], dtype=np.float32)
+            lin_vel_y_range = np.array(config["lin_vel_y_range"], dtype=np.float32)
+
+            ang_vel_range = np.array(config["ang_vel_range"], dtype=np.float32)
+            height_target_range = np.array(config["height_target_range"], dtype=np.float32)
+
+        # self.pad = gamepad.control_gamepad( num_commands, lin_vel_x_range, lin_vel_y_range, ang_vel_range, height_target_range, [1.0, 0.0, 3.14, 0.05])
+        self.low_cmd = unitree_go_msg_dds__LowCmd_()  
+        self.low_state = None  
+
+
+        self.controller_rt = 0.0
+        self.is_running = False
+
+        self.ang_vel_data = []
+        self.qtau_data = []
+        self.qtau_cmd = []
+
+        # thread handling
+        self.lowCmdWriteThreadPtr = None
+
+        # state
+        self.target_dof_pos = self.default_angles.copy()
+        self.target_dof_vel = np.zeros(NUM_MOTORS)
+        self.qpos = np.zeros(NUM_MOTORS, dtype=np.float32)
+        self.qvel = np.zeros(NUM_MOTORS, dtype=np.float32)
+        self.qtau = np.zeros(NUM_MOTORS, dtype=np.float32)
+        self.quat = np.zeros(4) # q_w q_x q_y q_z
+        self.ang_vel = np.zeros(3)
+
+        self.mode = ''
+
+
+        self.crc = CRC()
+
+    # Control methods
+    def Init(self):
+        self.InitLowCmd()
+
+        # create publisher #
+        self.lowcmd_publisher = ChannelPublisher("rt/lowcmd", LowCmd_)
+        self.lowcmd_publisher.Init()
+
+        # create subscriber # 
+        self.lowstate_subscriber = ChannelSubscriber("rt/lowstate", LowState_)
+        self.lowstate_subscriber.Init(self.LowStateMessageHandler, 10)
+
+        # Init default pos #
+        self.Start()
+
+        print("Initial Sucess !!!")
+
+    def get_gravity_orientation(self, quaternion):
+        qw = quaternion[0]
+        qx = quaternion[1]
+        qy = quaternion[2]
+        qz = quaternion[3]
+
+        gravity_orientation = np.zeros(3)
+
+        gravity_orientation[0] = 2 * (-qz * qx + qw * qy)
+        gravity_orientation[1] = -2 * (qz * qy + qw * qx)
+        gravity_orientation[2] = 1 - 2 * (qw * qw + qz * qz)
+
+        return gravity_orientation
+    
+
+    def Start(self):
+        self.is_running = True
+        self.lowCmdWriteThreadPtr = threading.Thread(target=self.LowCmdWrite)
+        self.lowCmdWriteThreadPtr.start()
+
+    def ShutDown(self):
+        self.is_running = False
+        self.lowCmdWriteThreadPtr.join()
+
+
+    # Private methods
+    def InitLowCmd(self):
+        self.low_cmd.head[0]=0xFE
+        self.low_cmd.head[1]=0xEF
+        self.low_cmd.level_flag = 0xFF
+        self.low_cmd.gpio = 0
+        for i in range(NUM_MOTORS):
+            self.low_cmd.motor_cmd[i].mode = 0x01  # (PMSM) mode
+            self.low_cmd.motor_cmd[i].q= self.sit_angles[i]
+            self.low_cmd.motor_cmd[i].kp = 0
+            self.low_cmd.motor_cmd[i].dq = 0.0
+            self.low_cmd.motor_cmd[i].kd = 0
+            self.low_cmd.motor_cmd[i].tau = 0
+
+    def LowStateMessageHandler(self, msg: LowState_):
+        self.low_state = msg
+        self.get_current_state()
+        # print(f'qpos {self.low_state.motor_state[0].q}')
+        # quat = self.low_state.imu_state.quaternion
+        # ang_vel = self.low_state.imu_state.gyroscope
+        # print(f'quat w: {quat[0]} x: {quat[1]} y: {quat[2]} z: {quat[3]}')
+        # print(f'ang_vel x: {ang_vel[0]} y: {ang_vel[1]} z: {ang_vel[2]}')
+    
+
+    def stand(self):
+        self.controller_rt += self.dt
+        ## Get into Default Joint pos ##
+        if (self.controller_rt < 3.0):
+            # Stand up in first 3 second
+            # Total time for standing up or standing down is about 1.2s
+            phase = np.tanh(self.controller_rt / 1.2)
+            for i in range(NUM_MOTORS):
+                self.low_cmd.motor_cmd[i].q = phase * self.default_angles[i] + (
+                    1 - phase) * self.sit_angles[i]
+                self.low_cmd.motor_cmd[i].kp = 25
+                self.low_cmd.motor_cmd[i].dq = 0.0
+                self.low_cmd.motor_cmd[i].kd = 0.5
+                self.low_cmd.motor_cmd[i].tau = 0.0
+    
+    def reset_timer(self):
+        self.controller_rt = 0.0
+        self.first_step = True
+        self.ang_vel_data = []
+        self.qtau_cmd = []
+        self.qtau_data = []
+    
+    def sit(self):
+        self.controller_rt += self.dt
+        ## Get into Default Joint pos ##
+        if (self.controller_rt < 3.0):
+            # Stand up in first 3 second
+            # Total time for standing up or standing down is about 1.2s
+            phase = np.tanh(self.controller_rt / 1.2)
+            for i in range(NUM_MOTORS):
+                self.low_cmd.motor_cmd[i].q = phase * self.sit_angles[i] + (
+                    1 - phase) * self.qpos[i]
+                self.low_cmd.motor_cmd[i].kp = 20
+                self.low_cmd.motor_cmd[i].dq = 0.0
+                self.low_cmd.motor_cmd[i].kd = 0.5
+                self.low_cmd.motor_cmd[i].tau = 0.0
+    
+
+    def stand_up(self):
+        self.mode = 'stand'
+        self.reset_timer()
+
+    def sit_down(self):
+        self.mode = 'sit'
+        self.reset_timer()
+    
+    def move_lqr(self):
+        self.mode = 'move'
+        self.reset_timer()
+    
+    def get_current_state(self):
+        for i in range(NUM_MOTORS):
+            self.qpos[i] = self.low_state.motor_state[i].q
+            self.qvel[i] = self.low_state.motor_state[i].dq
+            self.qtau[i] = self.low_state.motor_state[i].tau_est
+        
+        for i in range(3):
+            self.ang_vel[i] = self.low_state.imu_state.gyroscope[i]
+
+        for i in range(4):
+            self.quat[i] = self.low_state.imu_state.quaternion[i]
+
+        self.project_gravity = self.get_gravity_orientation(self.quat)
+
+    def move(self):
+        """ Implement controller here """
+        pass
+    
+
+
+    def LowCmdWrite(self):
+        
+        while self.is_running:
+            step_start = time.perf_counter()
+            if self.mode == 'stand':
+                self.stand()
+            elif self.mode == 'sit':
+                self.sit()
+            elif self.mode == 'move':
+                self.move()
+            self.low_cmd.crc = self.crc.Crc(self.low_cmd)
+            self.lowcmd_publisher.Write(self.low_cmd)
+
+            time_until_next_step = self.dt - (time.perf_counter() - step_start)
+            if time_until_next_step > 0:
+                time.sleep(time_until_next_step)
+        self.ResetParam()
+    
+        
+    def ResetParam(self):
+        self.controller_rt = 0
+        self.is_running = False
+
+
+if __name__ == '__main__':
+
+    print("WARNING: Please ensure there are no obstacles around the robot while running this example.")
+    input("Press Enter to continue...")
+
+    if len(sys.argv)>1:
+        ChannelFactoryInitialize(1, sys.argv[1])
+    else:
+        ChannelFactoryInitialize(1, "lo") # default DDS port for pineapple
+
+    controller = Controller()
+    controller.Init()
+
+    command_dict = {
+        "stand": controller.stand_up,
+        "sit": controller.sit_down,
+        # "move": controller.move_lqr,
+    }
+
+    while True:        
+        try:
+            cmd = input("CMD :")
+            if cmd in command_dict:
+                command_dict[cmd]()
+            elif cmd == "exit":
+                controller.ShutDown()
+                break
+
+        except Exception as e:
+            traceback.print_exc()
+            break
+    sys.exit(-1)     
